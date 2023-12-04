66e6d01bbdc1627f627801410898d55f64a89b02
M.Nihat Aydın <aydinmnihat2@gmail.com>
2023-12-04 13:06:21 +0300

    footer eklendi, buton hover eklendi.

--- page2/TODO.md
+++ page2/TODO.md
@@ -1,3 +1,5 @@
-[ ] Kaydet butonuna hover ekle
-[ ] CheckBox eklenebilir (Yasal hükümlülükleri kabul ıvır zıvırı.)
-[ ] Footer'a ikonları ekle
+[x] Kaydet butonuna hover ekle \n
+[ ] CheckBox eklenebilir (Yasal hükümlülükleri kabul ıvır zıvırı.) \n
+[ ] Footer'a ikonları ekle \n
+[ ] "Kaç Kuyu Hissesi Almak İstersiniz?" kısmında iç tarafta sağda
+çıkan yukarı aşağı butonu ya silinsin ya da custom edilsin \n
---
+++ page2/script.js
@@ -0,0 +1,15 @@
+const wellButton = document.querySelector("#well_button");
+const sacrificialButton = document.querySelector("#sacrificial_button");
+const saveButton = document.querySelector("#save_button");
+
+wellButton.addEventListener("click", () => {
+  console.log("wellButton is working!");
+});
+
+sacrificialButton.addEventListener("click", () => {
+  console.log("sacrificialButton is working!");
+});
+
+saveButton.addEventListener("click", () => {
+  console.log("saveButton is working!");
+});
