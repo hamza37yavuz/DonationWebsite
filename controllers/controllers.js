18b567e90673660eae8b7ef223229baffb651480
M.Nihat Aydın <aydinmnihat2@gmail.com>
2023-12-26 16:05:00 +0300

    login'nin büyük kısmı yapıldı.

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,5 +1,6 @@
 const { dbModel } = require("../models/models");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const getUsers = async (req, res) => {
   try {
@@ -48,9 +49,45 @@ const saveWaterWellDonation = async (res, req) => {
   } catch (error) {}
 };
 
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const { User } = await dbModel();
+
+    const user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      res.status(400).json({ error: "User not found.login" });
+      return;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      res.status(400).json({ error: "Password is incorrect.login" });
+      return;
+    }
+
+    const { id, username } = user;
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+      expiresIn: 30,
+    });
+
+    res.status(200).json({ msg: "User created", token });
+  } catch (error) {
+    console.error("Error login:", error);
+    res.status(500).json({ error: "Internal Server Error login" });
+  }
+};
+
 module.exports = {
   getUsers,
   createUser,
   getWaterWellQuantity,
   saveWaterWellDonation,
+  login,
 };
--- routes/routes.js
+++ routes/routes.js
@@ -5,10 +5,12 @@ const {
   getUsers,
   createUser,
   getWaterWellQuantity,
+  login,
 } = require("../controllers/controllers");
 
 router.route("/").get(getUsers);
 router.route("/").post(createUser);
 router.route("/donation").get(getWaterWellQuantity);
+router.route("/login").post(login);
 
 module.exports = router;
