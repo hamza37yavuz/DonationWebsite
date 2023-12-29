14fd9f29b653aca1cb8441a340c66160b048831f
Muhammet Hamza Yavuz <96301827+hamza37yavuz@users.noreply.github.com>
2023-12-29 14:07:06 +0300

    deleted private area

--- dbPrepare.js
+++ dbPrepare.js
@@ -6,8 +6,8 @@ const client = new Client({
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 5432,
   database: process.env.DB_DATABASE || "donation_db",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
+  user: process.env.DB_USER
+  password: process.env.DB_PASSWORD
 });
 
 const createTableQuery = `
